f1a7cdb8ad75a2b6271398e1f4d22fb65662dbfa
Christina <patriciasteffen@gmx.ch>
2021-05-04 12:15:08 +0200

    renamed other files from public to docs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 app.listen(port, () => {
     // eslint-disable-next-line no-console
