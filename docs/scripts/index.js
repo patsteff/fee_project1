c1462b0d0d9b8477ae509d087bf615a4c36d6ba9
Patricia <hirni5421@gmail.com>
2021-05-07 09:32:23 +0200

    commit zwischenstatus

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,1 +1,29 @@
 console.log('Hello World');
+
+const patricia = "das ist ein test";
+
+// toggleDarkMode on click of button
+function toggleDarkMode() {
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+const themeToggle = document.querySelector(".theme-toggle");
+themeToggle.addEventListener("click", toggleDarkMode);
+
+// add border and listen for clicks
+const note = document.querySelectorAll(".get-border");
+console.log(note);
+
+function toggleBorder() {
+    this.classList.toggle("add-border");
+}
+
+note.forEach(note => note.addEventListener("click", toggleBorder));
+
+// import external notes
+let notesList = {};
+fetch("/scripts/notes.json")
+    .then(blob => blob.json())
+    .then(data => notesList = data)
+    .catch((err) => console.error(err));
