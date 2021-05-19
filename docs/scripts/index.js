e25d3f13ba4a61635736d10c336090c3d82ee264
Christina <patriciasteffen@gmx.ch>
2021-05-19 15:51:35 +0200

    added create form, check layout again

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,40 +1,37 @@
-console.log('Hello World');
+console.log("Hello World");
 
 const patricia = "das ist ein test";
 
 // toggleDarkMode on click of button
 function toggleDarkMode() {
-    const element = document.body;
-    element.classList.toggle("dark-mode");
+  const element = document.body;
+  element.classList.toggle("dark-mode");
 }
 
 const themeToggle = document.querySelector(".theme-toggle");
-themeToggle.addEventListener("click", toggleDarkMode);
+themeToggle.addEventListener("change", toggleDarkMode);
 
 // add border and listen for clicks
 const note = document.querySelectorAll(".get-border");
 console.log(note);
 
 function toggleBorder() {
-    this.classList.toggle("add-border");
+  this.classList.toggle("add-border");
 }
 
-note.forEach(note => note.addEventListener("click", toggleBorder));
+note.forEach((note) => note.addEventListener("click", toggleBorder));
 
 // fetch external notes
 let notesList = [];
 fetch("/scripts/notes.json")
-    .then(blob => blob.json())
-    .then(data => notesList = data)
+  .then((blob) => blob.json())
+  .then((data) => (notesList = data));
 
 //
-setTimeout(
-    function() {
-        console.log(notesList);
-        console.log(typeof notesList);
-        console.log(notesList.title);
-        const titleNote = document.querySelector(".titleNote");
-        titleNote.innerHTML = notesList.title;
-
-    }
-    , 2000);
+setTimeout(function () {
+  console.log(notesList);
+  console.log(typeof notesList);
+  console.log(notesList.title);
+  const titleNote = document.querySelector(".titleNote");
+  titleNote.innerHTML = notesList.title;
+}, 2000);
