a72684b13b35a96a62c4bf1d1c2c17b0baf75ee8
Patricia S <patriciasteffen@gmx.ch>
2021-05-25 22:43:18 +0200

    added star priority

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -178,3 +178,15 @@ document
 
 // hide create note after "Save", comment for testing (add animation?)
 // document.querySelector("#save-note").addEventListener("click", hideNoteSection);
+
+// star rating
+/* let star = document.querySelectorAll('input');
+let showValue = document.querySelector('#rating-value');
+
+for (let i = 0; i < star.length; i++) {
+	star[i].addEventListener('click', function() {
+		i = this.value;
+
+		showValue.innerHTML = i + " out of 5";
+	});
+} */
